8b74b4bc330bb0694befe68cc7285f3297482d64
chameleon <martinkro@aliyun.com>
2017-04-16 10:25:38 +0800

    Animator

--- OpenGLExamples/HelloOpenGL/main.cpp
+++ OpenGLExamples/HelloOpenGL/main.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+static GLfloat spin = 0.0f;
+
 void RenderScene(void)
 {
 	glClearColor(1.0, 0.0, 0.0, 0.0);
@@ -10,15 +12,91 @@ void RenderScene(void)
 	glutSwapBuffers();
 }
 
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(spin, 0.0, 0.0, 1.0);
+	glColor3f(1.0, 1.0, 1.0);
+	glRectf(-25.0, -25.0, 25.0, 25.0);
+	glPopMatrix();
+	glutSwapBuffers();
+
+	/*
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(0.25, 0.25, 0.0);
+	glVertex3f(0.75, 0.25, 0.0);
+	glVertex3f(0.75, 0.75, 0.0);
+	glVertex3f(0.25, 0.75, 0.0);
+	glEnd();
+
+	glFlush();
+	*/
+}
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+	
+	/*
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	*/
+}
+
+void spinDisplay(void)
+{
+	spin = spin + 0.2f;
+	if (spin > 360.0)
+	{
+		spin = spin - 360.0f;
+	}
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	switch (button)
+	{
+	case GLUT_LEFT_BUTTON:
+		if (state == GLUT_DOWN)
+			glutIdleFunc(spinDisplay);
+		break;
+	case GLUT_MIDDLE_BUTTON:
+		if (state == GLUT_DOWN)
+			glutIdleFunc(NULL);
+		break;
+	default:
+		break;
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(500, 500);
-	glutInitWindowSize(800, 600);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(250, 250);
+	glutInitWindowSize(400, 400);
 	glutCreateWindow("OpenGL-Hello");
 
-	glutDisplayFunc(RenderScene);
+	init();
+
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
 	glutMainLoop();
 	return 0;
 }
